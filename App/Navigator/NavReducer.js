726c0c636e1f4001ba48c0dddce6dc4a0e40d250
barneyJomo <brnbsjm@gmail.com>
2017-02-17 03:39:00 +0300

    Solve route clash error(Duplicate Key)

--- App/Navigator/NavReducer.js
+++ App/Navigator/NavReducer.js
@@ -36,23 +36,5 @@ function navigationState(state = initialState, action) {
       return state;
   }
 }
-/*
-case PUSH_ROUTE:{
-
-     // trying to push the route where we already are, no need to change a thing
-         if (state.routes[state.index].key === (action.route && action.route.key))
-            return state;
-     // ensure no duplicate keys
-         const index = state.routes.findIndex((route) => {
-         return action.route.key === route.key && action.route.title ===route.title;
-        });
-         if (index > -1) {
-                  const clonedState = Object.assign({}, state);
-                  clonedState.routes.splice(index, 1);
-                  return NavigationStateUtils.push(clonedState, action.route);
-                  }
-      // normal case for a push
-            return NavigationStateUtils.push(state, action.route);
-*/
 
 export default navigationState;
