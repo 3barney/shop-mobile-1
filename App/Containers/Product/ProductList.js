bd06c039938715315eeac71a60fcc976e855ae24
barneyJomo <brnbsjm@gmail.com>
2017-02-17 14:54:49 +0300

    Move view To own component

--- App/Containers/Product/ProductList.js
+++ App/Containers/Product/ProductList.js
@@ -1,7 +1,8 @@
 import React, {Component, PropTypes} from 'react';
 import { connect } from 'react-redux';
-import { ListView, Row, GridRow, Image, View, Subtitle, Caption, Button, Icon, Text} from '@shoutem/ui';
+import { ListView, GridRow} from '@shoutem/ui';
 import * as _ from 'lodash';
+import ProductListRow from './ProductListRow';
 
 class ProductList extends Component {
 
@@ -13,24 +14,10 @@ class ProductList extends Component {
   _renderRow(data) {
     const cellView = _.map(data, (singleProduct) => {
       return (
-        <View key={singleProduct.id}>
-          <Row>
-            <Image
-              styleName="small rounded-corners"
-              source={{ uri: singleProduct.image}} />
-            <View styleName="vertical stretch space-between">
-              <Subtitle>{singleProduct.name}</Subtitle>
-              <Text numberOfLines={1}>{singleProduct.descriprion}</Text>
-              <View styleName="horizontal">
-                <Subtitle styleName="md-gutter-right">ksh {singleProduct.price}</Subtitle>
-                <Caption styleName="line-through">
-                  ksh { Math.floor(singleProduct.price) - 100}
-                </Caption>
-              </View>
-            </View>
-            <Button styleName="right-icon"><Icon name="add-to-cart" /></Button>
-          </Row>
-        </View>
+        <ProductListRow
+          key={singleProduct.id}
+          singleProduct={singleProduct}
+          onProductPress={this.props.onProductPress} />
       );
     });
     return (
@@ -54,7 +41,8 @@ class ProductList extends Component {
 }
 
 ProductList.propTypes = {
-  products: PropTypes.array.isRequired
+  products: PropTypes.array.isRequired,
+  onProductPress: PropTypes.func.isRequired
 };
 
 export default connect(null, null)(ProductList);
---
+++ App/Containers/Product/ProductListRow.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-use-before-define */
+import React, { PropTypes} from 'react';
+import { Row, Image, View, Subtitle, Caption, Button, Icon, Text } from '@shoutem/ui';
+import { TouchableOpacity } from 'react-native';
+
+const ProductListRow = ({singleProduct, onProductPress}) => {
+  return (
+    <View style={{flex: 1}}>
+      <TouchableOpacity
+        onPress={onProductPress}>
+        <Row>
+          <Image
+            styleName="small rounded-corners"
+            source={{ uri: singleProduct.image}} />
+          <View styleName="vertical stretch space-between">
+            <Subtitle>{singleProduct.name}</Subtitle>
+            <Text numberOfLines={1}>{singleProduct.descriprion}</Text>
+            <View styleName="horizontal">
+              <Subtitle styleName="md-gutter-right">ksh {singleProduct.price}</Subtitle>
+              <Caption styleName="line-through">
+                ksh { Math.floor(singleProduct.price) - 100}
+              </Caption>
+            </View>
+          </View>
+          <Button styleName="right-icon" onPress={() => console.log(singleProduct)}>
+            <Icon name="add-to-cart" />
+          </Button>
+        </Row>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+ProductListRow.propTypes = {
+  singleProduct: PropTypes.object.isRequired,
+  onProductPress: PropTypes.func.isRequired
+};
+
+export default ProductListRow;
